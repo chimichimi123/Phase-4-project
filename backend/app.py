9deb7a65e1b38627cb31ec596ed11bd61106ae12
Christopher Cantu <chrischimi02@gmail.com>
2024-05-30 11:19:33 -0500

    added cors

--- backend/app.py
+++ backend/app.py
@@ -5,21 +5,21 @@
 # Remote library imports
 from flask import request, Flask
 from flask_restful import Resource
+from flask_cors import CORS
 
 # Local imports
 from config import db, api, create_app
+from routes import register_routes  # Import the register_routes function
 
 app = create_app()
+CORS(app)
 
-# Add your model imports
-
-# Views go here!
+# Register the routes from routes.py
+register_routes(app)
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
 
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
