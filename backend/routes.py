571e0b3e3b7b215696d33c3fbccb1317b82ec328
Christopher Cantu <chrischimi02@gmail.com>
2024-05-30 12:50:26 -0500

    routes should work now I registered them all

--- backend/routes.py
+++ backend/routes.py
@@ -1,10 +1,10 @@
 # routes.py
 from flask import request, jsonify, Blueprint
-from models import db, User, Book, Review, UserBook, BookDetails
+from models import db, User, Book, Review, BookDetails
 
-bp = Blueprint('api', __name__)
+book_bp = Blueprint('book', __name__)
 
-@bp.route('/books', methods=['GET', 'POST'])
+@book_bp.route('/books', methods=['GET', 'POST'])
 def handle_books():
     if request.method == 'POST':
         data = request.get_json()
@@ -17,7 +17,7 @@ def handle_books():
     books = Book.query.all()
     return jsonify([book.to_dict() for book in books])
 
-@bp.route('/books/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+@book_bp.route('/books/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_book(id):
     book = Book.query.get_or_404(id)
     if request.method == 'GET':
@@ -35,7 +35,9 @@ def handle_book(id):
         db.session.commit()
         return '', 204
     
-@bp.route('/reviews', methods=['POST'])
+review_bp = Blueprint('review', __name__)
+    
+@review_bp.route('/reviews', methods=['POST'])
 def add_review():
     data = request.get_json()
     if not data:
@@ -45,7 +47,7 @@ def add_review():
     db.session.commit()
     return jsonify(new_review.to_dict()), 201
 
-@bp.route('/reviews/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+@review_bp.route('/reviews/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_review(id):
     review = Review.query.get_or_404(id)
     if request.method == 'GET':
@@ -62,8 +64,10 @@ def handle_review(id):
         db.session.delete(review)
         db.session.commit()
         return '', 204
+    
+user_bp = Blueprint('user', __name__)
 
-@bp.route('/users', methods=['GET', 'POST'])
+@user_bp.route('/users', methods=['GET', 'POST'])
 def handle_users():
     if request.method == 'POST':
         data = request.get_json()
@@ -76,7 +80,7 @@ def handle_users():
     users = User.query.all()
     return jsonify([user.to_dict() for user in users])
 
-@bp.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+@user_bp.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_user(id):
     user = User.query.get_or_404(id)
     if request.method == 'GET':
@@ -93,8 +97,10 @@ def handle_user(id):
         db.session.delete(user)
         db.session.commit()
         return '', 204
+    
+book_details_bp = Blueprint('book_details', __name__)
 
-@bp.route('/bookdetails', methods=['GET', 'POST'])
+@book_details_bp.route('/bookdetails', methods=['GET', 'POST'])
 def handle_bookdetails():
     if request.method == 'POST':
         data = request.get_json()
@@ -107,7 +113,7 @@ def handle_bookdetails():
     bookdetails = BookDetails.query.all()
     return jsonify([details.to_dict() for details in bookdetails])
 
-@bp.route('/bookdetails/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+@book_details_bp.route('/bookdetails/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_bookdetail(id):
     details = BookDetails.query.get_or_404(id)
     if request.method == 'GET':
@@ -126,4 +132,7 @@ def handle_bookdetail(id):
         return '', 204
 
 def register_routes(app):
-    app.register_blueprint(bp)
+    app.register_blueprint(book_details_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(book_bp)
+    app.register_blueprint(review_bp)
