63625d9295ea20367eefc1e09ef075d73aa909a8
Christopher Cantu <chrischimi02@gmail.com>
2024-05-30 11:32:32 -0500

    book details

---
+++ frontend/src/components/BookDetail.js
@@ -0,0 +1,42 @@
+// src/components/BookDetail.js
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ReviewForm from "./ReviewForm";
+
+function BookDetail() {
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/books/${id}`)
+      .then((response) => response.json())
+      .then((data) => setBook(data));
+  }, [id]);
+
+  if (!book) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{book.title}</h1>
+      <p>Author: {book.author}</p>
+      <p>Genre: {book.genre}</p>
+      <p>Summary: {book.summary}</p>
+      <img src={book.cover_image_url} alt={book.title} />
+
+      <h2>Reviews</h2>
+      <ul>
+        {book.reviews.map((review) => (
+          <li key={review.id}>
+            <p>Rating: {review.rating}</p>
+            <p>{review.comment}</p>
+            <p>By: {review.user.username}</p>
+          </li>
+        ))}
+      </ul>
+
+      <ReviewForm bookId={book.id} />
+    </div>
+  );
+}
+
+export default BookDetail;
--- frontend/src/components/BookList.js
+++ frontend/src/components/BookList.js
@@ -16,11 +16,11 @@ function BookList() {
       <ul>
         {books.map((book) => (
           <li key={book.id}>
-            {/* Link to the BookDetailPage for each book */}
+            {/* bookdetails page link */}
             <Link to={`/book/${book.id}`}>
               <h3>{book.title}</h3>
               <p>Author: {book.author}</p>
-              {/* Add additional book details as needed */}
+              {/* additional book details */}
             </Link>
           </li>
         ))}
